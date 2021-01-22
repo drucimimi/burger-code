32053044700665bc7f0f223dbf0a1d2fbe64c5c2
Drucilla Deroche <drucimimi@protonmail.com>
2021-01-22 17:08:40 +0100

    Update files

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Burger Code</title>
-    <script src="assets/js/jquery-3.5.1.js"></script>
-    <link rel="stylesheet" href="assets/css/bootstrap-3.3.7.css">
-    <script src="assets/js/bootstrap-3.3.7.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
     <link rel="stylesheet" href="assets/css/style.css" type="text/css">
   </head>
   <body>
@@ -47,7 +47,7 @@
           while($item = $statement->fetch()){
             echo '<div class="col-sm-6 col-md-4">
                     <div class="thumbnail">
-                      <img src="img/' . $item['image'] . '" alt="" width="276px" height="244px">
+                      <img src="assets/img/' . $item['image'] . '" alt="" width="276px" height="244px">
                       <div class="price">' . number_format($item['price'], 2, '.', '') . '</div>
                         <div class="caption">
                           <h4>' . $item['name'] . '</h4>
