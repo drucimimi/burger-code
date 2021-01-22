b2f5f33792f4be54fc231193dd55354b74276294
Drucilla Deroche <drucimimi@protonmail.com>
2021-01-22 12:17:06 +0100

    Move files

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Burger Code</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="assets/js/jquery-3.5.1.js"></script>
+    <link rel="stylesheet" href="assets/css/bootstrap-3.3.7.css">
+    <script src="assets/js/bootstrap-3.3.7.js"></script>
     <link rel="stylesheet" href="assets/css/style.css" type="text/css">
   </head>
   <body>
