0c4138e97c2d9c8ea186c6f2b3dca88120b6ab3b
Drucilla Deroche <drucimimi@protonmail.com>
2021-01-22 12:00:06 +0100

    Move files

--- index.php
+++ index.php
@@ -7,8 +7,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <link href='http://fonts.googleapis.com/css?family=Holtwood+One+SC' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css">
   </head>
   <body>
     <div class="container site">
